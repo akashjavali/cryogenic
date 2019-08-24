70e70a39ca57a8845b76c945d4558c32df5ab8c5
akashjavali <akash.j@pacewisdom.com>
2019-08-25 03:12:06 +0530

    init files

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/* Nav hover function */
+$('li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+/* Swiper JS */
+
+// var swiper = new Swiper('.swiper-container', {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
